7c29c2d381c15a75cbc822467bdc3901699817de
Petr <freeman12345678@list.ru>
2024-11-16 17:50:04 +0300

    game_store

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Swiper
+const swiper = new Swiper(".swiper", {
+   slidesPerGroup: 1,
+   loop: true,
+   pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+   },
+});
